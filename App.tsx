8853793e4549ceae1b89e0bc37684b2e5d7fdbf1
Maycon <mayconprogramador@gmail.com>
2022-04-13 20:00:28 -0300

    Commit 2

--- App.tsx
+++ App.tsx
@@ -1,21 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable, Image } from 'react-native';
 
 export default function App() {
+  function onPressLearnMore(){
+
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Clone Uber!</Text>
-      <StatusBar style="auto" />
+      <Image 
+        source={require('./images/motorista.png')}
+        style={{ width: 100, height: 100, marginTop: -250, marginBottom: 50 }}
+      />
+      <Text style={styles.title}>Clone Uber</Text>
+      <Text style={styles.text}>Bem vindo ao clone uber!</Text>
+      <Pressable style={styles.button} onPress={onPressLearnMore}>
+        <Text style={styles.text}>Começar</Text>
+    </Pressable>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    position: 'relative',
+    top: 0,
+    left: 0,
+
+    width: '100vw',
+    height: '100vh',
+
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgb(0, 0, 255)',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  button: {
+    position: 'absolute',
+    left: 'auto',
+    bottom: '10vh',
+    
+    backgroundColor: '#000',
+
+    width: '95vw',
+    height: '60px',
+
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  title: {
+    position: 'absolute',
+    left: 'auto',
+    top: '10vh',
+
+    color: '#fff',
+
+    fontSize: 30,
+    fontWeight: 500
+  },
+
+  text: {
+    color: '#fff',
+
+    fontSize: 20,
+  }
 });
